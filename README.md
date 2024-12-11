12d177843380a388676bfb4d8c91211eca8f9262
Javier <vichango@gmail.com>
2024-12-11 11:43:39 +0100

    Change template.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# ⚡️astro-bento-portfolio
+# README
 
-## A personal portfolio
+Other templates to try:
+
+- (Testing now) https://astro.build/themes/details/blog/ (OFFICIAL)
+    - From their README:
+        - https://github.com/Charca/astro-blog-template?tab=readme-ov-file
+- https://astro.build/themes/details/fuwari/
+- https://astro.build/themes/details/astro-simple-blog-theme/
+- https://astro.build/themes/details/milky-way/
+- https://astro.build/themes/details/portfolio/ (OFFICIAL)
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,10 @@
-import sitemap from "@astrojs/sitemap";
-import UnoCSS from "@unocss/astro";
-import icon from "astro-icon";
-import robotsTxt from "astro-robots-txt";
-import { defineConfig } from "astro/config";
-
-import solidJs from "@astrojs/solid-js";
-import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
-
-import svelte from "@astrojs/svelte";
+// @ts-check
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL || "http://localhost:4321",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://vichango.github.io/sitemap-index.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-    icon(),
-    svelte(),
-  ],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
-  // output: "server",
-  // adapter: netlify({ edgeMiddleware: true }),
-  vite: {
-    assetsInclude: "**/*.riv",
-  },
+	site: process.env.SITE_URL || "http://localhost:4321",
+	integrations: [mdx(), sitemap()],
 });
